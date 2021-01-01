0431878f887991adfdc285b3cceb87f3c6c11d5f
a.vafi <a.vafi>
2021-01-01 23:39:48 +0330

    added default enrichers, logged more request details, some enhancements

--- src/Serilog.AspNetCore/AspNetCore/RequestLoggingMiddleware.cs
+++ src/Serilog.AspNetCore/AspNetCore/RequestLoggingMiddleware.cs
@@ -14,12 +14,16 @@
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
+using Serilog.Debugging;
 using Serilog.Events;
 using Serilog.Extensions.Hosting;
 using Serilog.Parsing;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Serilog.AspNetCore
@@ -57,6 +61,7 @@ namespace Serilog.AspNetCore
             var collector = _diagnosticContext.BeginCollection();
             try
             {
+                await CaptureRequestBody(httpContext);
                 await _next(httpContext);
                 var elapsedMs = GetElapsedMilliseconds(start, Stopwatch.GetTimestamp());
                 var statusCode = httpContext.Response.StatusCode;
@@ -72,6 +77,32 @@ namespace Serilog.AspNetCore
             {
                 collector.Dispose();
             }
+        }
+
+        private async Task CaptureRequestBody(HttpContext httpContext)
+        {
+            if (httpContext.Request.ContentLength.HasValue && httpContext.Request.ContentLength > 0)
+            {
+                httpContext.Request.EnableBuffering();
+
+                using (StreamReader reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8, true, -1, true))
+                {
+                    var jsonString = await reader.ReadToEndAsync();
+                    try
+                    {
+                        httpContext.Items["RequestBody"] = jsonString;
+                        var data = JsonDocument.Parse(jsonString);
+                        httpContext.Items["RequestBodyObject"] = data;
+                    }
+                    catch (Exception ex)
+                    {
+                        SelfLog.WriteLine("Cannot read request body: " + ex.ToString());
+                    }
+                }
+
+                httpContext.Request.Body.Position = 0;
+            }
+
         }
 
         bool LogCompletion(HttpContext httpContext, DiagnosticContextCollector collector, int statusCode, double elapsedMs, Exception ex)
@@ -92,11 +123,38 @@ namespace Serilog.AspNetCore
             {
                 new LogEventProperty("RequestMethod", new ScalarValue(httpContext.Request.Method)),
                 new LogEventProperty("RequestPath", new ScalarValue(GetPath(httpContext))),
-                new LogEventProperty("StatusCode", new ScalarValue(statusCode)),
-                new LogEventProperty("Elapsed", new ScalarValue(elapsedMs))
+                new LogEventProperty("RequestRequestHost", new ScalarValue(httpContext.Request.Host.ToString())),
+                new LogEventProperty("RequestPathAndQuery", new ScalarValue(httpContext.Request.Path.ToString())),
+                new LogEventProperty("RequestScheme", new ScalarValue(httpContext.Request.Scheme)),
+                new LogEventProperty("RequestContentType", new ScalarValue(httpContext.Request.ContentType)),
+                new LogEventProperty("RequestProtocol", new ScalarValue(httpContext.Request.Protocol)),
+                new LogEventProperty("RequestQueryString", new ScalarValue(httpContext.Request.QueryString.ToString())),
+                new LogEventProperty("RequestQuery", new ScalarValue(statusCode)),
+                new LogEventProperty("RequestHeaders", new ScalarValue(statusCode)),
+                new LogEventProperty("ClientIpAddress", new ScalarValue(httpContext.Connection.RemoteIpAddress.ToString())),
+                new LogEventProperty("ResponseStatusCode", new ScalarValue(statusCode)),
+                new LogEventProperty("ResponseElapsedMiliSeconds", new ScalarValue(elapsedMs)),
             });
 
             var evt = new LogEvent(DateTimeOffset.Now, level, ex, _messageTemplate, properties);
+
+            if (statusCode >= 400 && httpContext.Response.Body != null)
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(httpContext.Response.Body, Encoding.UTF8, true, -1, true))
+                    {
+                        var responseText = reader.ReadToEnd();
+                        evt.AddOrUpdateProperty(new LogEventProperty("ResponseBody", new ScalarValue(responseText)));
+                    }
+
+                    httpContext.Response.Body.Position = 0;
+                }
+                catch (Exception)
+                {
+                }
+            }
+
             logger.Write(evt);
 
             return false;
