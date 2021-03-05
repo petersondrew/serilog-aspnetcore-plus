6698e6fd5648560bc8aabd6126ac8f84b35e6829
a.vafi <a.vafi>
2021-03-05 14:07:57 +0330

    added sample with log config file

--- samples/InlineInitializationSample/Program.cs
+++ samples/InlineInitializationSample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Formatting.Compact;
 using Serilog.Sinks.SystemConsole.Themes;
 
 namespace InlineInitializationSample
