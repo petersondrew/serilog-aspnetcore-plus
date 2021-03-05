a1900adeead48ec6ffacff87340dd4b44a963c04
a.vafi <a.vafi>
2021-03-05 13:21:27 +0330

    updated samples and README.md

---
+++ samples/InlineInitializationSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace InlineInitializationSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .UseSerilogPlus((configuration =>
+                {
+                    configuration
+                        .WriteTo.Debug()
+                        .WriteTo.Console(
+                            outputTemplate:
+                            "[{Timestamp:HH:mm:ss} {Level:u3}] {Message} {NewLine}{Properties} {NewLine}{Exception}{NewLine}",
+                            theme: SystemConsoleTheme.Literate);
+                }));
+    }
+}
